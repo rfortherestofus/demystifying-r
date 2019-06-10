bf0c49278fae2a2c148dce0cfdff365ae48f49a0
David Keyes <dgkeyes@gmail.com>
2019-06-10 16:10:33 -0700

    Ready for AEA coffee break

--- R/render.R
+++ R/render.R
@@ -2,22 +2,35 @@ library(tidyverse)
 library(lubridate)
 library(here)
 library(rmarkdown)
+library(webshot)
+library(fs)
 
 
 
 # Render report -----------------------------------------------------
 
 render(input = "report.Rmd", 
-       output_dir = here("reports"),
+       output_dir = here::here("reports"),
        output_file = paste0("report-", now(), ".html"),
        clean = T)
+
+# file_delete(here::here("report.html"))
 
 
 # Render slides -----------------------------------------------------
 
-render(input = here("slides", "slides.Rmd"), 
-       output_dir = here("slides"),
+render(input = here::here("slides", "slides.Rmd"), 
+       output_dir = here::here("slides"),
        output_file = "slides-aea.html",
        clean = T)
+
+# file_delete(here::here("slides", "slides.html"))
+
+# Convert to PDF ----------------------------------------------------------
+
+webshot(here::here("slides", "slides-aea.html"), 
+        here::here("slides", "slides-aea.pdf"),
+        vwidth = 1600,
+        vheight = 900) 
 
 
