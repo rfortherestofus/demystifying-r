b4d4c07bad87da09fc80a1e774899df8473ac68c
David Keyes <dgkeyes@gmail.com>
2019-11-07 13:42:54 -0800

    Post Vancouver webinar

--- R/render.R
+++ R/render.R
@@ -2,10 +2,8 @@ library(tidyverse)
 library(lubridate)
 library(here)
 library(rmarkdown)
-library(webshot)
+library(pagedown)
 library(fs)
-
-
 
 # Render report -----------------------------------------------------
 
@@ -16,21 +14,17 @@ render(input = "report.Rmd",
 
 # file_delete(here::here("report.html"))
 
-
 # Render slides -----------------------------------------------------
 
 render(input = here::here("slides", "slides.Rmd"), 
        output_dir = here::here("slides"),
-       output_file = "slides-dc-2019.html",
+       output_file = "slides-vancouver-2019.html",
        clean = T)
 
 # file_delete(here::here("slides", "slides.html"))
 
 # Convert to PDF ----------------------------------------------------------
 
-webshot(here::here("slides", "slides-aea.html"), 
-        here::here("slides", "slides-aea.pdf"),
-        vwidth = 1600,
-        vheight = 900) 
-
+chrome_print(here::here("slides", "slides-vancouver-2019.html"), 
+             here::here("slides", "slides-vancouver-2019.pdf"))
 
